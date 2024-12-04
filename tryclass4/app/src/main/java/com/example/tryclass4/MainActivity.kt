b4b04b6e7e49843694244c1c8244c16858ca2480
dthv201 <gaya.vishna@gmail.com>
2024-12-04 15:37:09 +0200

    start tic

---
+++ tryclass4/app/src/main/java/com/example/tryclass4/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tryclass4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tryclass4.ui.theme.TryClass4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+    }
+}
